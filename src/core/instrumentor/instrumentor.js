35efe30ed02b9b9b3a3bbd21bde38a704cc4594a
Javier Cabrera Arteaga <javier@Javiers-MacBook-Pro.local>
2019-05-22 19:29:58 +0200

    Reading file content in instrumentor call

--- src/core/instrumentor/instrumentor.js
+++ src/core/instrumentor/instrumentor.js
@@ -1,9 +1,26 @@
+const fs = require("fs");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse");
+const generator = require("@babel/generator");
 
 class Instrumentor{
 
     instrummentCode(path){
+        
+        const content = fs.readFileSync(path);
 
-        return 'Code instrumentation'
+        const ast = parser.parse(content.toString(), {
+            // parse in strict mode and allow module declarations
+            sourceType: "module",
+          
+            plugins: [
+              // enable jsx and flow syntax
+              //"jsx",
+              "flow"
+            ]
+          });
+
+        return generator.default(ast).code;
     }
 
 }
